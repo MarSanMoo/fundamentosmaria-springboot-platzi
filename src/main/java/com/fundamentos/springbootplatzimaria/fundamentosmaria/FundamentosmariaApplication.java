9fb9e015238ac3a3e519d44a7dd5228be8bedd3c
DESKTOP-LK5G5KM\Sanchez <Miguel Sanchez>
2022-06-23 14:19:27 -0500

    mi firtst commit

---
+++ src/main/java/com/fundamentos/springbootplatzimaria/fundamentosmaria/FundamentosmariaApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springbootplatzimaria.fundamentosmaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosmariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosmariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springbootplatzimaria/fundamentosmaria/FundamentosmariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springbootplatzimaria.fundamentosmaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosmariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
